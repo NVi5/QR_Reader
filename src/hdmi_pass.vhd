a8eb48fe0eb712de5454c9e3f8c34165e3f34114
NVi5 <nvader19@gmail.com>
2019-12-28 18:31:17 +0100

    Fixed tabs in hdmi_pass.vhd

--- src/hdmi_pass.vhd
+++ src/hdmi_pass.vhd
@@ -34,24 +34,24 @@ library UNISIM;
 use UNISIM.VComponents.all;
 
 entity hdmi_pass is
-    Port ( 
-	    sysclk_i         : in  std_logic; -- 125MHz clock
-		async_reset_i    : in  std_logic; -- reset
+    port ( 
+	sysclk_i         : in  std_logic; -- 125MHz clock
+	async_reset_i    : in  std_logic; -- reset
 
-		tmds_rx_clk_p_i  : in  std_logic;
-        tmds_rx_clk_n_i  : in  std_logic;
-        tmds_rx_data_p_i : in  std_logic_vector(2 downto 0);
-        tmds_rx_data_n_i : in  std_logic_vector(2 downto 0);
-		hdmi_rx_hpd_o    : out std_logic := '1'; -- HPD set high
+	tmds_rx_clk_p_i  : in  std_logic;
+	tmds_rx_clk_n_i  : in  std_logic;
+	tmds_rx_data_p_i : in  std_logic_vector(2 downto 0);
+	tmds_rx_data_n_i : in  std_logic_vector(2 downto 0);
+	hdmi_rx_hpd_o    : out std_logic := '1'; -- HPD set high
 
-		sda_io           : inout std_logic;
-		scl_io           : inout std_logic;
+	sda_io           : inout std_logic;
+	scl_io           : inout std_logic;
 
-        tmds_tx_clk_p_o  : out std_logic;
-        tmds_tx_clk_n_o  : out std_logic;
-        tmds_tx_data_p_o : out std_logic_vector(2 downto 0);
-        tmds_tx_data_n_o : out std_logic_vector(2 downto 0)	
-         );
+	tmds_tx_clk_p_o  : out std_logic;
+	tmds_tx_clk_n_o  : out std_logic;
+	tmds_tx_data_p_o : out std_logic_vector(2 downto 0);
+	tmds_tx_data_n_o : out std_logic_vector(2 downto 0)	
+	);
 end hdmi_pass;
 
 
@@ -74,68 +74,67 @@ component grey_convert is
 end component;
 
 component bw_convert is
-    port ( 
-    clk_in : in STD_LOGIC;
-    arst : in STD_LOGIC;
-    rgb_in : in STD_LOGIC_VECTOR (23 downto 0);
-    hsync_in : in STD_LOGIC;
-    vsync_in : in STD_LOGIC;
-    vde_in : in STD_LOGIC;
-    clk_out : out STD_LOGIC;
-    rgb_out : out STD_LOGIC_VECTOR (23 downto 0);
-    hsync_out : out STD_LOGIC;
-    vsync_out : out STD_LOGIC;
-    vde_out : out STD_LOGIC
-    );
+	port ( 
+	clk_in : in STD_LOGIC;
+	arst : in STD_LOGIC;
+	rgb_in : in STD_LOGIC_VECTOR (23 downto 0);
+	hsync_in : in STD_LOGIC;
+	vsync_in : in STD_LOGIC;
+	vde_in : in STD_LOGIC;
+	clk_out : out STD_LOGIC;
+	rgb_out : out STD_LOGIC_VECTOR (23 downto 0);
+	hsync_out : out STD_LOGIC;
+	vsync_out : out STD_LOGIC;
+	vde_out : out STD_LOGIC
+	);
 end component;
 
 component dvi2rgb_0
-  port (
-    TMDS_Clk_p    : in std_logic;
-    TMDS_Clk_n    : in std_logic;
-    TMDS_Data_p   : in std_logic_vector(2 downto 0);
-    TMDS_Data_n   : in std_logic_vector(2 downto 0);
-    RefClk        : in std_logic;
-    aRst          : in std_logic;
-    vid_pData     : out std_logic_vector(23 downto 0);
-    vid_pVDE      : out std_logic;   
-    vid_pHSync    : out std_logic;
-    vid_pVSync    : out std_logic;
-    PixelClk      : out std_logic;
-    aPixelClkLckd : out std_logic;
-    SDA_I         : in std_logic;
-    SDA_O         : out std_logic;
-    SDA_T         : out std_logic;
-    SCL_I         : in std_logic;
-    SCL_O         : out std_logic;
-    SCL_T         : out std_logic;
-    pRst          : in std_logic
-  );
+	port (
+	TMDS_Clk_p    : in std_logic;
+	TMDS_Clk_n    : in std_logic;
+	TMDS_Data_p   : in std_logic_vector(2 downto 0);
+	TMDS_Data_n   : in std_logic_vector(2 downto 0);
+	RefClk        : in std_logic;
+	aRst          : in std_logic;
+	vid_pData     : out std_logic_vector(23 downto 0);
+	vid_pVDE      : out std_logic;   
+	vid_pHSync    : out std_logic;
+	vid_pVSync    : out std_logic;
+	PixelClk      : out std_logic;
+	aPixelClkLckd : out std_logic;
+	SDA_I         : in std_logic;
+	SDA_O         : out std_logic;
+	SDA_T         : out std_logic;
+	SCL_I         : in std_logic;
+	SCL_O         : out std_logic;
+	SCL_T         : out std_logic;
+	pRst          : in std_logic
+	);
 end component;
 
 component rgb2dvi_0
-  PORT (
-    TMDS_Clk_p  : out std_logic;
-    TMDS_Clk_n  : out std_logic;
-    TMDS_Data_p : out std_logic_vector(2 downto 0);
-    TMDS_Data_n : out std_logic_vector(2 downto 0);
-    aRst        : in std_logic;
-    vid_pData   : in std_logic_vector(23 downto 0);
-    vid_pVDE    : in std_logic;
-    vid_pHSync  : in std_logic;
-    vid_pVSync  : in std_logic;
-    PixelClk    : in std_logic
-  );
+	port (
+	TMDS_Clk_p  : out std_logic;
+	TMDS_Clk_n  : out std_logic;
+	TMDS_Data_p : out std_logic_vector(2 downto 0);
+	TMDS_Data_n : out std_logic_vector(2 downto 0);
+	aRst        : in std_logic;
+	vid_pData   : in std_logic_vector(23 downto 0);
+	vid_pVDE    : in std_logic;
+	vid_pHSync  : in std_logic;
+	vid_pVSync  : in std_logic;
+	PixelClk    : in std_logic
+	);
 end component;
 
 component clk_wiz_0
-port
- (
-  clk_out1          : out    std_logic;
-  reset             : in     std_logic;
-  locked            : out    std_logic;
-  clk_in1           : in     std_logic
- );
+	port (
+	clk_out1          : out    std_logic;
+	reset             : in     std_logic;
+	locked            : out    std_logic;
+	clk_in1           : in     std_logic
+	);
 end component;
 
 signal vid_pData          : std_logic_vector(23 downto 0);
@@ -185,31 +184,31 @@ grey_convert_inst : grey_convert
     );
     
 bw_convert_inst : bw_convert
-        port map ( 
-        clk_in => PixelClk_grey,
-        arst => async_reset_i,
-        rgb_in => vid_pData_grey,
-        hsync_in => vid_pHSync_grey,
-        vsync_in => vid_pVSync_grey,
-        vde_in => vid_pVDE_grey,
-        clk_out => PixelClk_BW,
-        rgb_out => vid_pData_BW,
-        hsync_out => vid_pHSync_BW,
-        vsync_out => vid_pVSync_BW,
-        vde_out => vid_pVDE_BW
-        );
+	port map ( 
+	clk_in => PixelClk_grey,
+	arst => async_reset_i,
+	rgb_in => vid_pData_grey,
+	hsync_in => vid_pHSync_grey,
+	vsync_in => vid_pVSync_grey,
+	vde_in => vid_pVDE_grey,
+	clk_out => PixelClk_BW,
+	rgb_out => vid_pData_BW,
+	hsync_out => vid_pHSync_BW,
+	vsync_out => vid_pVSync_BW,
+	vde_out => vid_pVDE_BW
+	);
 
 
 clkwiz_inst : clk_wiz_0
-   port map ( 
-   clk_out1 => clk_200M,            
-   reset   => async_reset_i,
-   locked  => locked,
-   clk_in1 => sysclk_i
- );
+	port map ( 
+	clk_out1 => clk_200M,            
+	reset   => async_reset_i,
+	locked  => locked,
+	clk_in1 => sysclk_i
+	);	
 
 dvi2rgb_inst : dvi2rgb_0
-  port map (
+	port map (
     TMDS_Clk_p    => tmds_rx_clk_p_i,
     TMDS_Clk_n    => tmds_rx_clk_n_i,
     TMDS_Data_p   => tmds_rx_data_p_i,
@@ -229,7 +228,7 @@ dvi2rgb_inst : dvi2rgb_0
     SCL_O         => scl_o,
     SCL_T         => scl_t,
     pRst          => '0'   -- Active high PixelClk synchronous reset
-  );
+	);
   
 SDA_IOBUF_inst: IOBUF
     generic map(
@@ -258,7 +257,7 @@ SCL_IOBUF_inst: IOBUF
     ); 
     
 rgb2dvi_inst : rgb2dvi_0
-  port map (
+	port map (
     TMDS_Clk_p  => tmds_tx_clk_p_o,
     TMDS_Clk_n  => tmds_tx_clk_n_o,
     TMDS_Data_p => tmds_tx_data_p_o,
@@ -269,6 +268,6 @@ rgb2dvi_inst : rgb2dvi_0
     vid_pHSync  => vid_pHSync_BW,
     vid_pVSync  => vid_pVSync_BW,
     PixelClk    => PixelClk_BW
-  );
+	);
 
 end Structural;
